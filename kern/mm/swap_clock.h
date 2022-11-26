df518ee066367521ef6b3be3f3712160d54e2ea7
Xuyao Wang <wxy835283116@gmail.com>
2022-11-27 00:35:37 +0800

    finish extended clock

---
+++ kern/mm/swap_clock.h
@@ -0,0 +1,11 @@
+//
+// Created by yao on 11/27/22.
+//
+
+#ifndef U_CORE_SWAP_CLOCK_H
+#define U_CORE_SWAP_CLOCK_H
+
+#include <swap.h>
+extern struct swap_manager swap_manager_clock;
+
+#endif //U_CORE_SWAP_CLOCK_H
